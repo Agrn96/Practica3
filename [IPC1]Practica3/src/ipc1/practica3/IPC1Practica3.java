033cfb6abdc594ac964ac08cd68a7772ba92ac67
Alberto Reyes <agrn96p@gmail.com>
2019-10-25 16:16:37 -0600

    Primer Commit
    Agregando proyecto a GitHub

---
+++ [IPC1]Practica3/src/ipc1/practica3/IPC1Practica3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipc1.practica3;
+
+/**
+ *
+ * @author 201612174 
+ */
+public class IPC1Practica3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
