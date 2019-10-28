db7e6470fca4b02675b36edab1ad16a6dfc6cadb
Alberto Reyes <agrn96p@gmail.com>
2019-10-28 15:21:34 -0600

    2nd Commit
    +Agregado Input de un documento
    +GUI creado
    
    -Agregar output al JTextArea
    -Agregar funcionalidad al GUI

---
+++ [IPC1]Practica3/src/ipc1/practica3/CustomOutputStream.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipc1.practica3;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author 201612174 <Alberto Gabriel Reyes Ning>
+ */
+public class CustomOutputStream extends OutputStream {
+
+    private JTextArea textArea;
+
+    public CustomOutputStream(JTextArea textArea) {
+        this.textArea = textArea;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        // redirects data to the text area
+        textArea.append(String.valueOf((char) b));
+        // scrolls the text area to the end of data
+        textArea.setCaretPosition(textArea.getDocument().getLength());
+    }
+}
--- [IPC1]Practica3/src/ipc1/practica3/IPC1Practica3.java
+++ [IPC1]Practica3/src/ipc1/practica3/IPC1Practica3.java
@@ -5,9 +5,14 @@
  */
 package ipc1.practica3;
 
+import UI_Layout.UI_Area;
+import java.util.Scanner;
+import java.io.*;
+import javax.swing.JTextArea;
+
 /**
  *
- * @author 201612174 
+ * @author 201612174
  */
 public class IPC1Practica3 {
 
@@ -15,7 +20,43 @@ public class IPC1Practica3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Nombre de documento
+        String fileName = "numeros.txt";
+        
+        //String para guardar informacion del documento
+        String[] att = null;
+        
+        //Guardando informacion del documento en un String Array
+        try {
+            FileReader inputFile = new FileReader(fileName);
+            try {
+                Scanner parser = new Scanner(inputFile);
+                while (parser.hasNextLine()) {
+                    //Delimitador ","
+                    String line = parser.nextLine();
+                    att = line.split(",");
+                }
+
+            } finally {
+                //Cerrar documento
+                inputFile.close();
+            }
+            //Errors
+        } catch (FileNotFoundException exception) {
+            System.out.println(fileName + " not found");
+        } catch (IOException exception) {
+            System.out.println("Unexpected I/O error occured.");
+        }
+        
+        UI_Area temp = new UI_Area();
+        temp.setVisible(true);
+        //Verificacion que el String esta initializado.
+        for (int i = 0; i < att.length; i++) {
+            System.out.println(att[i] + " ");
+        }
+
+        
     }
-    
 }
+
+
