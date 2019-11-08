9ea405e0eb972fa5db48c73f957180819962768c
Alberto Reyes <agrn96p@gmail.com>
2019-11-08 14:25:41 -0600

    5th Commit
    Terminado parte(Posible listas de priorida para cambiar codigo)

--- [IPC1]Practica3/src/ipc1/practica3/IPC1Practica3.java
+++ [IPC1]Practica3/src/ipc1/practica3/IPC1Practica3.java
@@ -6,7 +6,6 @@
 package ipc1.practica3;
 
 import UI_Layout.UI_Area;
-import java.util.Scanner;
 import java.io.*;
 
 /**
@@ -15,16 +14,23 @@ import java.io.*;
  */
 public class IPC1Practica3 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) throws IOException {
         // Nombre de documento
         String fileName = "archivo_entrada.txt";
-        inicio(fileName);
+        Lista_Circular lc = new Lista_Circular();
+        //Cambiar por lista doble enlazada
+        Lista_Circular ld = new Lista_Circular();
+        inicio(fileName, lc, ld);
+
+        //Probando input en el terminal, quitar al finalizar tarea
+        lc.display();
+
+        //Abrir GUI
+        UI_Area temp = new UI_Area(lc, ld);
+        temp.setVisible(true);
     }
-    
-    public static void inicio(String fileName) throws IOException {
+
+    public static void inicio(String fileName, Lista_Circular lc, Lista_Circular ld) throws IOException {
         String[] name = null;
         try {
             FileReader fr = new FileReader(fileName);
@@ -32,11 +38,16 @@ public class IPC1Practica3 {
             String linea;
             while ((linea = br.readLine()) != null) {
                 name = linea.split(",");
-                System.out.println(name[0] + " | " + name[1] + " | " + name[2]);            }
+
+                //Agregar a las dos listas;
+                if (Integer.parseInt(name[0]) % 2 == 1) {
+                    lc.add(Integer.parseInt(name[0]), name[1], Boolean.parseBoolean(name[2]));
+                } else {//Cambiar por lista doble enlazada
+                    ld.add(Integer.parseInt(name[0]), name[1], Boolean.parseBoolean(name[2]));
+                }
+            }
         } catch (FileNotFoundException exception) {
             System.out.println(fileName + " not found");
-
         }
-
     }
 }
